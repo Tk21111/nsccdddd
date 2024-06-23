c282c3c2af69d3e4c264ab698168c24fbd5aa8f6
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2024-06-23 11:26:07 +0700

    instruction

--- App.js
+++ App.js
@@ -5,52 +5,7 @@ import { randFood , rerand } from './hook/rand';
 import {setUser , updateUser} from './hook/user';
 
 const App = () => {
-  /*
-  const jsonFilePath = `${FileSystem.documentDirectory}data.json`;
-
-  const createAndWriteFile = async () => {
-    const data = { message: 'Hello, this is a JSON file!' };
-    const content = JSON.stringify(data);
-
-    try {
-      await FileSystem.writeAsStringAsync(jsonFilePath, content);
-      console.log('JSON file written successfully!');
-    } catch (error) {
-      console.error('Error writing JSON file:', error);
-    }
-  };
-
-  const readFile = async () => {
-    try {
-      const content = await FileSystem.readAsStringAsync(jsonFilePath);
-      const data = JSON.parse(content);
-      console.log('JSON file content:', data);
-    } catch (error) {
-      console.error('Error reading JSON file:', error);
-    }
-  };
-
-  const updateFile = async () => {
-    const updatedData = { message: 'Updated content!' };
-    const updatedContent = JSON.stringify(updatedData);
-
-    try {
-      await FileSystem.writeAsStringAsync(jsonFilePath, updatedContent);
-      console.log('JSON file updated successfully!');
-    } catch (error) {
-      console.error('Error updating JSON file:', error);
-    }
-  };
-
-  const deleteFile = async () => {
-    try {
-      await FileSystem.deleteAsync(jsonFilePath);
-      console.log('JSON file deleted successfully!');
-    } catch (error) {
-      console.error('Error deleting JSON file:', error);
-    }
-  };
-  */
+  
   x = {"test" : "test"}
   return (
     <View>
