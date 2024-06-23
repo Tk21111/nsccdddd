073514485fce8e5e3f07f0bf6139877fe4fd0a1b
Tk21111 <101468538+Tk21111@users.noreply.github.com>
2024-06-23 19:54:53 +0700

    fix addfood to list to match the structure

--- hook/list.js
+++ hook/list.js
@@ -1,5 +1,19 @@
 import { createAndWriteFile , readFile , updateFile , deleteFile } from "../fileManagement";
 
 const foodListUpdate = async (data) => {
-    await updateFile('food.json' , data)
+    let prv = await readFile('food.json');
+
+    if (data.cal >= 2000){
+        prv.list_2000.push(data)
+    } else if (data.cal >=1500){
+        prv.list_1500.push(data)
+    } else if (data.cal >=1000){
+        prv.list_1000.push(data)
+    } else if (data.cal >=500){
+        prv.list_500.push(data)
+    }
+
+    
+
+    await createAndWriteFile('food.json' , prv)
 }
